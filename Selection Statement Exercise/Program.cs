19b2d4e8d17d02b2a3304a52c00f2ea33a9e5e8f
Donald Johnson <donaldrjohnson66@gmail.com>
2022-11-20 18:45:08 -0600

    Created main

---
+++ Selection Statement Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Selection_Statement_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
